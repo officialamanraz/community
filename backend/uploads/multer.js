47ceed03a59bc91302c2cc7e9d62f76c54e4171a
Aman <officialamanraz@gmail.com>
2026-08-15 04:33:57 -0700

    fixed cloudinary storage and formats typo

--- backend/uploads/multer.js
+++ backend/uploads/multer.js
@@ -1,22 +1,21 @@
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
-const {CloudinaryStorage} = require('multer-storage-cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary:cloudinary,
-    params:{
-        folder:'community_app_images',
-        allowed_formate:['jpg','jpeg','png','webp']
+    cloudinary: cloudinary,
+    params: {
+        folder: 'community_app_images',
+        allowedFormats: ['jpg', 'jpeg', 'png', 'webp'] 
     }
 });
 
-const upload = multer({ storage: storage,
- });
+const upload = multer({ storage: storage });
 
 module.exports = upload;
