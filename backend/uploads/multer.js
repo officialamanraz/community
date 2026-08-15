0757f0283466f34eaab3347c14a424a50040c33b
Aman <officialamanraz@gmail.com>
2026-08-15 04:21:28 -0700

    fixed cloudinary storage constructor error

--- backend/uploads/multer.js
+++ backend/uploads/multer.js
@@ -1,15 +1,15 @@
 const multer = require('multer');
-const cloudnary = require('cloudinary').v2;
-const {CloudnaryStorage} = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
+const {CloudinaryStorage} = require('multer-storage-cloudinary');
 
-cloudnary.config({
+cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET
 });
 
-const storage = new CloudnaryStorage({
-    cloudnary:cloudnary,
+const storage = new CloudinaryStorage({
+    cloudinary:cloudinary,
     params:{
         folder:'community_app_images',
         allowed_formate:['jpg','jpeg','png','webp']
