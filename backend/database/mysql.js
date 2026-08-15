413373c523729b7080c6922d6879234f2b2bb8e5
Aman <officialamanraz@gmail.com>
2026-08-14 23:01:47 -0700

    fix update

--- backend/database/mysql.js
+++ backend/database/mysql.js
@@ -1,16 +1,26 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config(); // .env file load karne ke liye
 
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+//     connectTimeout: 20000 
+// });
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    uri: process.env.DATABASE_URL, // <-- 5 alag variables ki jagah ab sirf ye single URL
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    connectTimeout: 20000 
+    connectTimeout: 20000,
+    ssl: {
+        rejectUnauthorized: false // <-- Aiven cloud database se securely connect hone ke liye zaroori
+    }
 });
 
 module.exports = pool;
